3e478c9dcc0ececee1a0348d0372b430cfcd49a5
dl <pavel.velichenko@gmail.com>
2025-06-04 16:37:46 +0300

    feat: user + token creation

---
+++ app/Console/Commands/MakeUserCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use function Laravel\Prompts\password;
+use function Laravel\Prompts\text;
+
+class MakeUserCommand extends Command
+{
+    protected $signature = 'make:user';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $user = User::create([
+            'name' => text(
+                label: 'Enter your name: ',
+                placeholder: 'John Doe',
+                required: true,
+            ),
+            'email' => text(
+                label: 'Enter your email: ',
+                placeholder: 'john.doe@example.com',
+                required: true,
+                validate: [
+                    'email' => 'email|unique:users,email',
+                ]
+            ),
+            'password' => password(
+                label: 'Enter your password: ',
+                placeholder: '********',
+                required: true,
+            )
+        ]);
+        $this->info("User created successfully: {$user->name} ({$user->email}) with ID {$user->id}");
+    }
+}
---
+++ app/Console/Commands/MakeUserTokenCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class MakeUserTokenCommand extends Command
+{
+    protected $signature = 'make:user-token {userId}';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $user = User::findOrFail($this->argument('userId'));
+        $token = $user->createToken('User Token')->plainTextToken;
+        $this->info("User token created successfully: {$token}");
+    }
+}
